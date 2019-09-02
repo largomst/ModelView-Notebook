a347651eb5e681e9bf87a038f5eeec1e881e0afb
largomst <dingrui.zhen@gmail.com>
2019-09-02 16:02:32 +0800

    use index

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ list_ = QtWidgets.QListView(splitter)
 list_.setModel(model)
 list_.setRootIndex(model.index(QtCore.QDir.currentPath()))
 
+parentIndex = model.index(QtCore.QDir.currentPath())  # 获取当前路径的 Index
+parentIndex = model.index(0, 0, QtCore.QModelIndex())  # 获取当前路径的 Index
+numRows = model.rowCount(parentIndex)
+print(numRows)
+
+for row in range(numRows):
+    index = model.index(row, 0, parentIndex)
+    text = model.data(index, QtGui.Qt.DisplayRole)
+    print(text)
+
+
 splitter.setWindowTitle("Two Views")
 splitter.show()
 
